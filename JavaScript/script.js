b107bd85769494614387944c1ea6443e03f6881a
ALFREDOBUMBA\david <alfredobumba3@gmail.com>
2024-04-17 16:55:32 +0100

    Initialized git repository.

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+// Função para verificar se o documento está pronto
+$(document).ready(function() {
+    // Adiciona interatividade aos botões 'Ver mais'
+    $('.btn-primary').click(function(event) {
+        event.preventDefault(); // Previne o comportamento padrão do link
+        var cardTitle = $(this).closest('.card-body').find('.card-title').text();
+        alert('Mais informações sobre: ' + cardTitle);
+    });
+});
+
+$(document).ready(function() {
+    $('#googleMapsBtn').click(function(event) {
+      event.preventDefault();
+      alert('Você será redirecionado para o Google Maps.');
+      window.open($(this).attr('href'));
+    });
+});
